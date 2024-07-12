c3bc4a81b8f0f8766addd9469d4943d07697954d
khantact <kevinhan0@gmail.com>
2024-07-11 20:49:06 -0400

    formatting changes via prettier

--- app/_components/Facts.tsx
+++ app/_components/Facts.tsx
@@ -1,26 +1,28 @@
 export default function Facts() {
-    return (
-        <div id="facts" className='page component pt-0'>
-            <div className='row justify-evenly h-full w-full'>
-                <div className="max-md:w-[90%]">
-                    <p className='text-2xl text-red-700'>Hackers</p>
-                    <p className='text-7xl text-red-500'>100</p>
-                </div>
-                <div className="max-md:w-[90%] max-md:mt-10">
-                    <p className='text-2xl text-amber-600'>Projects</p>
-                    <p className='text-7xl text-amber-500'>100</p>
-                </div>
-                <div className="max-md:w-[90%] max-md:mt-10">
-                    <p className='text-2xl text-blue-700'>Workshops</p>
-                    <p className='text-7xl text-blue-600'>100</p>
-                </div>
-                <div className="max-md:w-[90%] max-md:mt-10">
-                    <p className='text-2xl text-green-700'>Prizes</p>
-                    <p className='text-7xl text-green-600'>$15000</p>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div id="facts" className="page component pt-0">
+			<div className="row justify-evenly h-full w-full select-none">
+				<div className="max-md:w-[90%]">
+					<p className="text-2xl text-red-700">Hackers</p>
+					<p className="text-7xl text-red-500">100</p>
+				</div>
+				<div className="max-md:w-[90%] max-md:mt-10">
+					<p className="text-2xl text-amber-600">Projects</p>
+					<p className="text-7xl text-amber-500">100</p>
+				</div>
+				<div className="max-md:w-[90%] max-md:mt-10">
+					<p className="text-2xl text-blue-700">Workshops</p>
+					<p className="text-7xl text-blue-600">100</p>
+				</div>
+				<div className="max-md:w-[90%] max-md:mt-10">
+					<p className="text-2xl text-green-700 text-center">
+						Prizes
+					</p>
+					<p className="text-7xl text-green-600">$15000</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 // We'll add details about the judging soon, but it'll be a wonderful opportunity for you to play the games your fellow jammers have made as well as talk about some of the fun things you built! Examples of categories we'll be looking at are:
@@ -33,7 +35,6 @@ export default function Facts() {
 // Masterful Mechanics - Macropads
 // Ostentatiously Offline - Board Games :)
 // Stay tuned for more & look at our 1.0 recap!
-
 
 //https://github.com/descope/nextjs-hackathon-template/blob/main/app/page.tsx
 // https://www.angelhacks.org/
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,30 @@
-import About from './_components/About'
-import Facts from './_components/Facts'
-import Intro from './_components/Intro'
+import About from "./_components/About";
+import Facts from "./_components/Facts";
+import Intro from "./_components/Intro";
 // import Speakers from './_components/Speakers'
-import FAQ from './_components/FAQ'
+import FAQ from "./_components/FAQ";
 // import Sponsors from './_components/Sponsors'
-import Schedule from './_components/Schedule'
+import Schedule from "./_components/Schedule";
 
-import { ScheduleList } from './_data/Schedule'
+import { ScheduleList } from "./_data/Schedule";
 
+import { AboutParagraph } from "./_data/About";
+import { FAQList } from "./_data/FAQ";
 
-import { AboutParagraph } from './_data/About'
-import { FAQList } from './_data/FAQ'
+import Link from "next/link";
 
-import Link from 'next/link';
-
-import DynamicNavbar from './_components/DynamicNavbar';
-
+import DynamicNavbar from "./_components/DynamicNavbar";
 
 export default function Home() {
-  return (
-    <div className='page h-full'>
-      <div className='w-[90%]'>
-        <Intro />
-        <Facts />
-        <About data={AboutParagraph} />
-        <Schedule data={ScheduleList} />
-        <FAQ data={FAQList} />
-
-      </div>
-    </div>
-  )
+	return (
+		<div className="page h-full">
+			<div className="w-[90%]">
+				<Intro />
+				<Facts />
+				<About data={AboutParagraph} />
+				<Schedule data={ScheduleList} />
+				<FAQ data={FAQList} />
+			</div>
+		</div>
+	);
 }
